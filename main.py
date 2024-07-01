95c8df48d37a10788dc49bd18008db9199c0a206
Dominik Phillip Theilen <dominik.phillip.theilen@sap.com>
2024-07-01 13:23:05 +0200

    Add threat safety

--- main.py
+++ main.py
@@ -59,149 +59,151 @@ def grl_train_and_test():
         ProgramStats.SUM_OF_TIMESAFE = 0
         ProgramParams.SIMULATION_DATE += timedelta(1)
 
+# Only execute script when main process is called
+if __name__ == '__main__':
 
-# Read program params
-if os.path.isfile("execution/program_params.csv"):
-    with open("execution/program_params.csv", mode="r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            ProgramParams.set_member(row["parameter"], row["value"])
-# Read concurrency params
-if os.path.isfile("execution/concurrency_params.csv"):
-    with open("execution/concurrency_params.csv", mode="r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            ConcurrencyParams.set_member(row["parameter"], row["value"])
-# Read execution file
-if os.path.isfile("execution/run.csv"):
-    with open("execution/run.csv", mode="r") as file:
-        reader = csv.DictReader(file)
-        if reader.__next__()["Command"] == "grl":
+    # Read program params
+    if os.path.isfile("execution/program_params.csv"):
+        with open("execution/program_params.csv", mode="r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                ProgramParams.set_member(row["parameter"], row["value"])
+    # Read concurrency params
+    if os.path.isfile("execution/concurrency_params.csv"):
+        with open("execution/concurrency_params.csv", mode="r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                ConcurrencyParams.set_member(row["parameter"], row["value"])
+    # Read execution file
+    if os.path.isfile("execution/run.csv"):
+        with open("execution/run.csv", mode="r") as file:
+            reader = csv.DictReader(file)
+            if reader.__next__()["Command"] == "grl":
+                ProgramParams.EXECUTION_MODE = Mode.GRAPH_REINFORCEMENT_LEARNING
+                if reader.__next__()["Command"] == "train_and_test":
+                    grl_train_and_test()
+                    exit()
+
+
+    while True:
+        user_input = input(
+            "Which menu you want to enter? (Graph Reinforcement Learning -> 1, Static Data Generation -> 2, Visualization -> 3, Data Analysis -> 4) "
+        )
+        if user_input == "1":
             ProgramParams.EXECUTION_MODE = Mode.GRAPH_REINFORCEMENT_LEARNING
-            if reader.__next__()["Command"] == "train_and_test":
-                grl_train_and_test()
-                exit()
+            while True:
+                user_input = input(
+                    "Which script do you want to start? (Online Training and Testing -> 1, Start Graph Reinforcement Learning (one day) -> 2) "
+                )
+                if user_input == "1":
+                    grl_train_and_test()
+                    break
+                if user_input == "2":
+                    initialize_vehicle_positions()
+                    execute_graph_reinforcement_learning()
+                    break
+                else:
+                    print("This option is not allowed. Please try again.")
+            break
 
+        elif user_input == "2":
+            while True:
+                user_input = input(
+                    "Which script do you want to start? (Create Zone Graph -> 1, Create Public Transport Graph -> 2) "
+                )
+                if user_input == "1":
+                    create_zone_graph()
+                    break
+                if user_input == "2":
+                    generate_shortest_paths_graph()
+                    break
+                else:
+                    print("This option is not allowed. Please try again.")
+            break
 
-while True:
-    user_input = input(
-        "Which menu you want to enter? (Graph Reinforcement Learning -> 1, Static Data Generation -> 2, Visualization -> 3, Data Analysis -> 4) "
-    )
-    if user_input == "1":
-        ProgramParams.EXECUTION_MODE = Mode.GRAPH_REINFORCEMENT_LEARNING
-        while True:
-            user_input = input(
-                "Which script do you want to start? (Online Training and Testing -> 1, Start Graph Reinforcement Learning (one day) -> 2) "
-            )
-            if user_input == "1":
-                grl_train_and_test()
-                break
-            if user_input == "2":
-                initialize_vehicle_positions()
-                execute_graph_reinforcement_learning()
-                break
-            else:
-                print("This option is not allowed. Please try again.")
-        break
+        elif user_input == "3":
+            while True:
+                user_input = input(
+                    "Which script do you want to start? (Visualize Zone Graph -> 1, Visualize vehicle positions -> 2) "
+                )
+                if user_input == "1":
+                    visualize_zone_graph()
+                    break
+                elif user_input == "2":
+                    visualize_vehicle_positions()
+                    break
+                else:
+                    print("This option is not allowed. Please try again.")
+            break
 
-    elif user_input == "2":
-        while True:
-            user_input = input(
-                "Which script do you want to start? (Create Zone Graph -> 1, Create Public Transport Graph -> 2) "
-            )
-            if user_input == "1":
-                create_zone_graph()
-                break
-            if user_input == "2":
-                generate_shortest_paths_graph()
-                break
-            else:
-                print("This option is not allowed. Please try again.")
-        break
+        elif user_input == "4":
+            while True:
+                print("Please remind to adapt to the correct paths.")
+                user_input = input(
+                    "Which script do you want to start? (Numerical analysis -> 1, Graphical analysis -> 2) "
+                )
 
-    elif user_input == "3":
-        while True:
-            user_input = input(
-                "Which script do you want to start? (Visualize Zone Graph -> 1, Visualize vehicle positions -> 2) "
-            )
-            if user_input == "1":
-                visualize_zone_graph()
-                break
-            elif user_input == "2":
-                visualize_vehicle_positions()
-                break
-            else:
-                print("This option is not allowed. Please try again.")
-        break
-
-    elif user_input == "4":
-        while True:
-            print("Please remind to adapt to the correct paths.")
-            user_input = input(
-                "Which script do you want to start? (Numerical analysis -> 1, Graphical analysis -> 2) "
-            )
-
-            if user_input == "1":
-                while True:
-                    user_input = input(
-                        "Which script do you want to start? (Numerical data analysis -> 1, Numerical data comparison -> 2, Numerical selected multi data comparison -> 3, Numerical multi data comparison -> 4) "
-                    )
-                    if user_input == "1":
-                        numerical_analysis()
-                        break
-                    elif user_input == "2":
-                        numerical_comparison()
-                        break
-                    elif user_input == "3":
-                        numerical_selected_multi_comparison()
-                        break
-                    elif user_input == "4":
-                        numerical_multi_comparison()
-                        break
-                    else:
-                        print("This option is not allowed. Please try again.")
-                break
-            
-            elif user_input == "2":
-                while True:
-                    user_input = input(
-                        "Which script do you want to start? (Data analysis -> 1, Data comparison -> 2) "
-                    )
-                    if user_input == "1":
-                        while True:
-                            user_input = input(
-                                "Which script do you want to start? (\n   Plot average time reduction -> 1\n   Plot average trip distance for direct routes -> 2\n   Plot average trip distance for combination routes -> 3\n   Plot vehicle distribution -> 4\n   Plot combi route ratio -> 5\n   Plot workload -> 6\n) "
-                            )
-                            if user_input == "1":
-                                plt.average_time_reduction_per_day()
-                                break
-                            elif user_input == "2":
-                                plt.average_trip_distances_per_day_for_direct_routes()
-                                break
-                            elif user_input == "3":
-                                plt.average_trip_distances_per_day_for_combination_routes()
-                                break
-                            elif user_input == "4":
-                                plt.visualize_vehicles()
-                                break
-                            elif user_input == "5":
-                                plt.visualize_combi_route_ratio()
-                                break
-                            elif user_input == "6":
-                                plt.visualize_workload()
-                                break
-                            else:
-                                print("This option is not allowed. Please try again.")
-                        break
-                    elif user_input == "2":
-                        plot_comparison()
-                        break
-                    else:
-                        print("This option is not allowed. Please try again.")
-                break
+                if user_input == "1":
+                    while True:
+                        user_input = input(
+                            "Which script do you want to start? (Numerical data analysis -> 1, Numerical data comparison -> 2, Numerical selected multi data comparison -> 3, Numerical multi data comparison -> 4) "
+                        )
+                        if user_input == "1":
+                            numerical_analysis()
+                            break
+                        elif user_input == "2":
+                            numerical_comparison()
+                            break
+                        elif user_input == "3":
+                            numerical_selected_multi_comparison()
+                            break
+                        elif user_input == "4":
+                            numerical_multi_comparison()
+                            break
+                        else:
+                            print("This option is not allowed. Please try again.")
+                    break
                 
-    
-        break
+                elif user_input == "2":
+                    while True:
+                        user_input = input(
+                            "Which script do you want to start? (Data analysis -> 1, Data comparison -> 2) "
+                        )
+                        if user_input == "1":
+                            while True:
+                                user_input = input(
+                                    "Which script do you want to start? (\n   Plot average time reduction -> 1\n   Plot average trip distance for direct routes -> 2\n   Plot average trip distance for combination routes -> 3\n   Plot vehicle distribution -> 4\n   Plot combi route ratio -> 5\n   Plot workload -> 6\n) "
+                                )
+                                if user_input == "1":
+                                    plt.average_time_reduction_per_day()
+                                    break
+                                elif user_input == "2":
+                                    plt.average_trip_distances_per_day_for_direct_routes()
+                                    break
+                                elif user_input == "3":
+                                    plt.average_trip_distances_per_day_for_combination_routes()
+                                    break
+                                elif user_input == "4":
+                                    plt.visualize_vehicles()
+                                    break
+                                elif user_input == "5":
+                                    plt.visualize_combi_route_ratio()
+                                    break
+                                elif user_input == "6":
+                                    plt.visualize_workload()
+                                    break
+                                else:
+                                    print("This option is not allowed. Please try again.")
+                            break
+                        elif user_input == "2":
+                            plot_comparison()
+                            break
+                        else:
+                            print("This option is not allowed. Please try again.")
+                    break
+                    
+        
+            break
 
-    else:
-        print("This option is not allowed. Please try again.")
+        else:
+            print("This option is not allowed. Please try again.")
